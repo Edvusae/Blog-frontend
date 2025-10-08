8c6b7a0b48441c680d9defffbb767e150e0c864c
Edvusae <105985332+Edvusae@users.noreply.github.com>
2025-10-08 16:12:51 +0200

    feat: Refactoring the PostCard.jsx

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+// This component renders a card for an individual blog post
 function PostCard({ post }) {
+  // Destructure the post prop to get individual post details
+  const { title, author, date, content, id } = post;
+
   return (
     <div className="card mb-4">
+      {<img src={post.image} alt={title} className="card-img-top" />}
       <div className="card-body">
-        <h5 className="card-title">{post.title}</h5>
+        <h5 className="card-title">{title}</h5>
         <h6 className="card-subtitle mb-2 text-muted">
-          By {post.author} on {post.date}
+          By {author} on {date}
         </h6>
         <p className="card-text">
-          {post.content.substring(0, 100)}... {/* Display a short snippet */}
+          {content.substring(0, 100)}... {/* Display a short snippet */}
         </p>
-        <Link to={`/posts/${post.id}`} className="btn btn-primary">
+        <Link to={`/posts/${id}`} className="btn btn-primary">
           Read More
         </Link>
       </div>
